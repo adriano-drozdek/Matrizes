001ed88cc2193220ee3e0030c806ce2529224fa0
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-19 15:06:39 -0300

    Informando a quantidade de numeros negativos.

--- src/programa/Matriz.java
+++ src/programa/Matriz.java
@@ -10,22 +10,38 @@ public class Matriz {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Quantidade de linha: ");
+		System.out.print("Quantidade de linha/coluna: ");
 		int n = sc.nextInt();
 		int[][] mat = new int[n][n];
-		
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
+
+		System.out.println();
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
-		
-		System.out.println("Diagonal principal:");
-		for (int i=0; i<mat.length; i++) {
+
+		System.out.println();
+		System.out.print("Diagonal principal: ");
+		for (int i = 0; i < mat.length; i++) {
 			System.out.print(mat[i][i] + " ");
 		}
-		
-		
+
+		System.out.println();
+
+		int quant = 0;
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] < 0) {
+					quant++;
+				}
+			}
+		}
+
+		System.out.println();
+		System.out.println("Numeros negativos: " + quant);
+
 		sc.close();
 	}
 
