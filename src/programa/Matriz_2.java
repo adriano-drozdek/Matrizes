7196d3de120136adc3dc32e082c8ef4047fc56de
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-19 15:58:07 -0300

    Iniciando a matriz na memória.

---
+++ src/programa/Matriz_2.java
@@ -0,0 +1,18 @@
+package programa;
+
+import java.util.Scanner;
+
+public class Matriz_2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int[][] mat = new int[m][n];
+
+		sc.close();
+	}
+
+}
