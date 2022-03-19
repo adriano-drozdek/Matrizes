c99a1d7400fdc21e260fff51c9a51977bbf0e467
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-19 14:54:20 -0300

    Informando a diagonal principal.

--- src/programa/Matriz.java
+++ src/programa/Matriz.java
@@ -10,14 +10,21 @@ public class Matriz {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Quantidade de linha: ");
 		int n = sc.nextInt();
 		int[][] mat = new int[n][n];
-
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < 0; j++) {
+		
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
+		
+		System.out.println("Diagonal principal:");
+		for (int i=0; i<mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		
 		
 		sc.close();
 	}
