e7dcf048ba27590f6dff17f477a297f72f01c97a
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-24 12:23:23 -0300

    Método para verificação de posição

--- src/programa/Matriz_2.java
+++ src/programa/Matriz_2.java
@@ -8,6 +8,7 @@ public class Matriz_2 {
 
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Informe a quantidade de  linhas e colunas: ");
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		int[][] mat = new int[m][n];
@@ -19,7 +20,31 @@ public class Matriz_2 {
 				mat[i][j] = sc.nextInt();
 			}
 		}
+		System.out.println();
+		System.out.print("Informe o numero para consulta de posição: ");
 
+		System.out.println();
+		int x = sc.nextInt();
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == x) {
+					System.out.println("Posição " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Esquerda: " + mat[i][j - 1]);
+					}
+					if (i > 0) {
+						System.out.println("acima: " + mat[i - 1][j]);
+					}
+					if (j < mat[i].length - 1) {
+						System.out.println("Direita: " + mat[i][j + 1]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("Abaixo: " + mat[i + 1][j]);
+					}
+				}
+			}
+		}
 		sc.close();
 	}
 
